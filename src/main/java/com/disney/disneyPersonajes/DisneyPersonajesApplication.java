ed7c9b94a857c7918770782ada7b6e9bb07eab78
georgmarys <georgmarys1993@gmail.com>
2021-10-27 00:36:19 -0300

    Entitys commit

---
+++ src/main/java/com/disney/disneyPersonajes/DisneyPersonajesApplication.java
@@ -0,0 +1,13 @@
+package com.disney.disneyPersonajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyPersonajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyPersonajesApplication.class, args);
+	}
+
+}
